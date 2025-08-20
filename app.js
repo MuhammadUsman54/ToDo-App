5017a6b3f73329b1e6a2f9d363939d0c15fa6c69
Muhammad Usman <usmannoor758236@gmail.com>
2025-08-20 10:45:42 +0500

    Updated

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 var toDos = [];
+var editIndex = null;
 
+// Add Todo
 function addTodo() {
-    var userInput = document.getElementById("todo")
+  var userInput = document.getElementById("todo");
 
-   if(userInput.value.trim() === ""){
-     alert("Please Enter Any Task!")
-     return;
-   }
+  if (userInput.value.trim() === "") {
+    alert("Please Enter Any Task!");
+    return;
+  }
 
-     toDos.push(userInput.value);
-   userInput.value = ""
-
-
-    showTodos();
+  toDos.push(userInput.value);
+  userInput.value = "";
+  showTodos();
 }
 
+// Show Todos
 function showTodos() {
-    var userList = document.getElementById("list-item")
+  var userList = document.getElementById("list-item");
+  userList.innerHTML = "";
 
-    userList.innerHTML = "";
-
-    for (var i = 0; i < toDos.length; i++) {
-        userList.innerHTML += `
+  for (var i = 0; i < toDos.length; i++) {
+    userList.innerHTML += `
           <li>
             ${toDos[i]}
             <div class="actions">
@@ -30,20 +30,42 @@ function showTodos() {
             </div>
           </li>
         `;
-      }
-    }
-
-function deleteTodo(index){
-    toDos.splice(index,1) 
-    showTodos()
+  }
 }
 
+// Delete Todo
+function deleteTodo(index) {
+  toDos.splice(index, 1);
+  showTodos();
+}
+
+// Edit Todo
 function editTodo(index) {
-    var newValue = prompt("Edit your task:", toDos[index]);
-    if (newValue !== null && newValue.trim() !== "") {
-        toDos[index] = newValue;
-        showTodos();
-    }
+  var userInput = document.getElementById("todo");
+  var addBtn = document.getElementById("addBtn");
+  var updateBtn = document.getElementById("updateBtn");
+  userInput.value = toDos[index];
+  editIndex = index;
+  addBtn.style.display = "none";
+  updateBtn.style.display = "inline-block";
 }
 
+// updateTodo
+function updateTodo() {
+  var userInput = document.getElementById("todo");
+  var addBtn = document.getElementById("addBtn");
+  var updateBtn = document.getElementById("updateBtn");
 
+  if (userInput.value.trim() === "") {
+    alert("Task cannot be empty!");
+    return;
+  }
+
+  toDos[editIndex] = userInput.value;
+  editIndex = null;
+  userInput.value = "";
+  addBtn.style.display = "none";
+  updateBtn.style.display = "inline-block";
+
+  showTodos();
+}
